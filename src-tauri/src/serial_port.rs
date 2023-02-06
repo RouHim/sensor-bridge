a06f32e3c26d96ec3b0b6c6170dc7ad8c4405338
Rouven <Rouven@DESKTOP-A130QIL>
2023-02-06 20:07:06 +0100

    feat: remove animations and add multi address sending

--- src-tauri/src/serial_port.rs
+++ src-tauri/src/serial_port.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt::format;
 use std::io::Write;
 use std::sync::{Arc, Mutex};
@@ -7,7 +8,7 @@ use std::time::Duration;
 use serialport::*;
 
 use crate::aida64;
-use crate::config::ComPortConfig;
+use crate::config::{ComPortConfig, OutputConfig};
 
 /// Opens a serial port and returns a handle to it.
 pub fn open(port_name: &str, baud_rate: u32) -> Box<dyn SerialPort> {
@@ -21,36 +22,74 @@ pub fn list_ports() -> Vec<SerialPortInfo> {
     available_ports().expect("No ports found!")
 }
 
+/// Starts a new thread that writes to the serial port.
+/// Returns a handle to the thread.
+/// The thread will be stopped when the port_running_state_handle is set to false.
+/// The thread will be joined when the handle is dropped.
+/// Two different data commands are supported:
+/// - c<address_id>,<address_id>,...: Replaced with the specified addresses. Used to configure the receiving arduino.
+/// - d<sensor_value>,<sensor_value>,...: Replaced with the specified sensor values to send to the previous configured addresses.
+/// Each data command must end with a semicolon (;).
+/// The data command is sent to the serial port at the specified push rate.
 pub fn start_sync(com_port_config: ComPortConfig, port_running_state_handle: Arc<Mutex<bool>>) -> Arc<thread::JoinHandle<()>> {
     // Start new thread that writes to the serial port, as configured in to config
     let handle = std::thread::spawn(move || {
-        let mut com_port = open(&com_port_config.com_port, com_port_config.baud_rate);
-        let vec = com_port_config.output_config.values().collect::<Vec<_>>();
-        let output_config = vec.get(0).unwrap();
+        let com_port_name = com_port_config.com_port;
+        let baud_rate = com_port_config.baud_rate;
+        let push_rate = com_port_config.push_rate as u64;
+
+        // Open the named serial port with specified baud rate
+        let mut com_port = open(&com_port_name, baud_rate);
+
+        let output_configs = com_port_config.output_config;
+
+        configure_output_addresses(&mut com_port, &output_configs);
+
+        let data_template = output_configs
+            .values()
+            .map(|output_config| output_config.data_config.clone())
+            .collect::<Vec<String>>()
+            .join(",");
 
         while *port_running_state_handle.lock().unwrap() {
+            // Read sensor values
             let sensors = aida64::read_sensors();
 
-            // Replace all sensor ids from sensor values with the curly brackets sensor ids in the output config
-            let mut data = output_config.data_config.clone();
+            // Replace all sensor ids with their values
+            let mut data_template = data_template.clone();
             for sensor in sensors.iter() {
-                data = data.replace(
+                data_template = data_template.replace(
                     format!("[{}]", &sensor.id).as_str(),
                     sensor.value.to_string().as_str(),
                 );
             }
 
-            let data = format!("{};", data);
-            println!("Writing data: {} -> {}", com_port_config.com_port, data);
+            // Write data to serial port
+            let data = format!("d{};", data_template);
+            println!("Sending data: {}", data);
             com_port.write_all(data.as_bytes()).unwrap();
-            thread::sleep(Duration::from_millis(com_port_config.push_rate as u64));
+
+            // Wait for next push
+            thread::sleep(Duration::from_millis(push_rate));
         }
 
-        println!("Parking port: {}", com_port_config.com_port);
+        // Wait for thread to be joined
         thread::park();
-
-        println!("Closing port: {}", com_port_config.com_port);
     });
 
     Arc::new(handle)
+}
+
+/// Tells the receiving arduino which output addresses to use in which order.
+/// All following data will be sent to the specified addresses in this order.
+/// The arduino will then send the data to the specified addresses.
+fn configure_output_addresses(com_port: &mut Box<dyn SerialPort>, output_config: &HashMap<String, OutputConfig>) {
+    // Join address string into one comma separated string
+    let addresses: Vec<String> = output_config.keys().cloned().collect();
+    let addresses = addresses.join(",");
+
+    // Send configure command to arduino
+    let configure_command_data = format!("c{};", addresses);
+    println!("Sending configure command: {}", configure_command_data);
+    com_port.write_all(configure_command_data.as_bytes()).unwrap();
 }
