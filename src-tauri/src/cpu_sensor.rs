670da3b0f4f1df9d2bed4dd21b000fadf94c1e63
Rouven Himmelstein <rouven@himmelstein.info>
2023-05-26 17:22:24 +0200

    feat: add individual cpu load sensor value

--- src-tauri/src/cpu_sensor.rs
+++ src-tauri/src/cpu_sensor.rs
@@ -1,5 +1,6 @@
 use crate::sensor;
 use crate::sensor::SensorValue;
+use rayon::prelude::*;
 use std::thread;
 use std::time::Duration;
 use systemstat::platform::PlatformImpl;
@@ -13,30 +14,48 @@ impl sensor::SensorProvider for CpuSensor {
     }
 
     fn get_sensor_values(&self) -> Vec<SensorValue> {
-        let mut sensors = vec![];
-
         let system_stat = System::new();
 
-        // TODO: Request all sensors in parallel using a par_iter
-        let bla = |system_stat| get_total_cpu_load(system_stat);
+        let sensors_requests = vec![get_total_cpu_load, get_individual_cpu_load];
 
-        sensors.push(bla(&system_stat));
-
-        sensors
+        sensors_requests
+            .par_iter()
+            .flat_map(|f| f(&system_stat))
+            .collect()
     }
 }
 
-fn get_total_cpu_load(system_stat: &PlatformImpl) -> SensorValue {
+fn get_total_cpu_load(system_stat: &PlatformImpl) -> Vec<SensorValue> {
     let cpu_load = system_stat.cpu_load_aggregate().unwrap();
     thread::sleep(Duration::from_millis(250));
     let cpu = cpu_load.done().unwrap();
     let total_cpu_load =
         cpu.user * 100.0 + cpu.nice * 100.0 + cpu.system * 100.0 + cpu.interrupt * 100.0;
 
-    SensorValue {
+    vec![SensorValue {
         id: "cpu_load_total".to_string(),
         value: format!("{:.2}%", total_cpu_load),
         label: "Total CPU load".to_string(),
         sensor_type: "percentage".to_string(),
-    }
+    }]
+}
+
+fn get_individual_cpu_load(system_stat: &PlatformImpl) -> Vec<SensorValue> {
+    let cpu_load = system_stat.cpu_load().unwrap();
+    thread::sleep(Duration::from_millis(250));
+    let cpus = cpu_load.done().unwrap();
+
+    cpus.into_iter()
+        .enumerate()
+        .map(|(i, cpu)| {
+            let total_cpu_load =
+                cpu.user * 100.0 + cpu.nice * 100.0 + cpu.system * 100.0 + cpu.interrupt * 100.0;
+            SensorValue {
+                id: format!("cpu_load_{}", i),
+                value: format!("{:.2}%", total_cpu_load),
+                label: format!("CPU {} load", i),
+                sensor_type: "percentage".to_string(),
+            }
+        })
+        .collect()
 }
