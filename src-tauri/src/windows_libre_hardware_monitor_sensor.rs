3b458e8e1ad34a7829fd3995d922caa9656ef961
Rouven Himmelstein <rouven@himmelstein.info>
2023-07-17 15:36:46 +0200

    chore(app): cleanup

--- src-tauri/src/windows_libre_hardware_monitor_sensor.rs
+++ src-tauri/src/windows_libre_hardware_monitor_sensor.rs
@@ -8,6 +8,7 @@ pub fn get_sensor_values() -> Vec<SensorValue> {
 
 #[cfg(target_os = "windows")]
 fn get_all_available_sensors() -> Vec<SensorValue> {
+    // http://openhardwaremonitor.org/wordpress/wp-content/uploads/2011/04/OpenHardwareMonitor-WMI.pdf
     // TODO: Implement this
 }
 
