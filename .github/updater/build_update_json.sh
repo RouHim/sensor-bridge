3e1f6d912009ed4c2cdf7775f2eb0e45709095f8
Rouven Himmelstein <rouven@himmelstein.info>
2023-08-16 16:01:34 +0200

    fix(pipeline): gist file upload

--- .github/updater/build_update_json.sh
+++ .github/updater/build_update_json.sh
@@ -13,13 +13,14 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 set -e
 
-UPDATER_FILE_NAME=sensorbridge_update.json
-
 VERSION=$1
+UPDATER_JSON_FILE=$2
 NOW=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 NOTES=$(jq -r '.head_commit.message' "$GITHUB_EVENT_PATH")
 
-echo "The update message is: '${NOTES}'"
+echo "Version: $VERSION"
+echo "Updater JSON file: $UPDATER_JSON_FILE"
+echo "NOTES: $NOTES"
 
 # Collect linux AppImage data
 APPIMAGE_URL="https://github.com/RouHim/sensor-bridge/releases/download/v${VERSION}/sensor-bridge_${VERSION}_amd64.AppImage.tar.gz"
@@ -30,14 +31,14 @@ MSI_URL=https://github.com/RouHim/sensor-bridge/releases/download/v${VERSION}/se
 MSI_SIG=$(curl -L --silent "https://github.com/RouHim/sensor-bridge/releases/download/v${VERSION}/sensor-bridge_${VERSION}_x64_en-US.msi.zip.sig")
 
 # Set the generic version, notes and pub_date
-cat <<< $(jq --arg VERSION "$VERSION" '.version = "\($VERSION)"' ${UPDATER_FILE_NAME}) > ${UPDATER_FILE_NAME}
-cat <<< $(jq --arg NOTES "$NOTES" '.notes = "\($NOTES)"' ${UPDATER_FILE_NAME}) > ${UPDATER_FILE_NAME}
-cat <<< $(jq --arg NOW "$NOW" '.pub_date = "\($NOW)"' ${UPDATER_FILE_NAME}) > ${UPDATER_FILE_NAME}
+cat <<< $(jq --arg VERSION "$VERSION" '.version = "\($VERSION)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
+cat <<< $(jq --arg NOTES "$NOTES" '.notes = "\($NOTES)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
+cat <<< $(jq --arg NOW "$NOW" '.pub_date = "\($NOW)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
 
 # Set the linux specific data
-cat <<< $(jq --arg APPIMAGE_URL "$APPIMAGE_URL" '.platforms."linux-x86_64".url = "\($APPIMAGE_URL)"' ${UPDATER_FILE_NAME}) > ${UPDATER_FILE_NAME}
-cat <<< $(jq --arg APPIMAGE_SIG "$APPIMAGE_SIG" '.platforms."linux-x86_64".signature = "\($APPIMAGE_SIG)"' ${UPDATER_FILE_NAME}) > ${UPDATER_FILE_NAME}
+cat <<< $(jq --arg APPIMAGE_URL "$APPIMAGE_URL" '.platforms."linux-x86_64".url = "\($APPIMAGE_URL)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
+cat <<< $(jq --arg APPIMAGE_SIG "$APPIMAGE_SIG" '.platforms."linux-x86_64".signature = "\($APPIMAGE_SIG)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
 
 # Set the windows specific data
-cat <<< $(jq --arg MSI_URL "$MSI_URL" '.platforms."windows-x86_64".url = "\($MSI_URL)"' ${UPDATER_FILE_NAME}) > ${UPDATER_FILE_NAME}
-cat <<< $(jq --arg MSI_SIG "$MSI_SIG" '.platforms."windows-x86_64".signature = "\($MSI_SIG)"' ${UPDATER_FILE_NAME}) > ${UPDATER_FILE_NAME}
+cat <<< $(jq --arg MSI_URL "$MSI_URL" '.platforms."windows-x86_64".url = "\($MSI_URL)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
+cat <<< $(jq --arg MSI_SIG "$MSI_SIG" '.platforms."windows-x86_64".signature = "\($MSI_SIG)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
