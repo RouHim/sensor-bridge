190231b4f298fe8b1b983dc4cc9e4052557e149e
Rouven Himmelstein <rouven@himmelstein.info>
2023-08-25 08:30:05 +0200

    chore(pipeline): extract repo name

--- .github/updater/build_update_json.sh
+++ .github/updater/build_update_json.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2046
 #
 # Description:
-#   This script is used to build the update json file for sensor-bridge.
+#   This script is used to build the update json file.
 #
 # Parameters:
 #   $1: The version of the release without v
@@ -17,31 +17,33 @@ VERSION=$1
 UPDATER_JSON_FILE=$2
 NOW=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 NOTES=$(jq -r '.head_commit.message' "$GITHUB_EVENT_PATH")
+REPO_BASE_URL="https://github.com/RouHim/sensor-bridge"
 
 echo "Version: $VERSION"
 echo "Updater JSON file: $UPDATER_JSON_FILE"
 echo "NOTES: $NOTES"
+echo "Repo:"
 
 # Collect linux AppImage data
-APPIMAGE_URL="https://github.com/RouHim/sensor-bridge/releases/download/v${VERSION}/sensor-bridge_${VERSION}_amd64.AppImage.tar.gz"
-APPIMAGE_SIG=$(curl -L --silent "https://github.com/RouHim/sensor-bridge/releases/download/v${VERSION}/sensor-bridge_${VERSION}_amd64.AppImage.tar.gz.sig")
+APPIMAGE_URL="${REPO_BASE_URL}/releases/download/v${VERSION}/sensor-bridge_${VERSION}_amd64.AppImage.tar.gz"
+APPIMAGE_SIG=$(curl -L --silent "${REPO_BASE_URL}/releases/download/v${VERSION}/sensor-bridge_${VERSION}_amd64.AppImage.tar.gz.sig")
 
 # Collect windows MSI data
-MSI_URL=https://github.com/RouHim/sensor-bridge/releases/download/v${VERSION}/sensor-bridge_${VERSION}_x64_en-US.msi.zip
-MSI_SIG=$(curl -L --silent "https://github.com/RouHim/sensor-bridge/releases/download/v${VERSION}/sensor-bridge_${VERSION}_x64_en-US.msi.zip.sig")
+MSI_URL=${REPO_BASE_URL}/releases/download/v${VERSION}/sensor-bridge_${VERSION}_x64_en-US.msi.zip
+MSI_SIG=$(curl -L --silent "${REPO_BASE_URL}/releases/download/v${VERSION}/sensor-bridge_${VERSION}_x64_en-US.msi.zip.sig")
 
 # Set the generic version, notes and pub_date
-cat <<< $(jq --arg VERSION "$VERSION" '.version = "\($VERSION)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
-cat <<< $(jq --arg NOTES "$NOTES" '.notes = "\($NOTES)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
-cat <<< $(jq --arg NOW "$NOW" '.pub_date = "\($NOW)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
+cat <<<$(jq --arg VERSION "$VERSION" '.version = "\($VERSION)"' ${UPDATER_JSON_FILE}) >${UPDATER_JSON_FILE}
+cat <<<$(jq --arg NOTES "$NOTES" '.notes = "\($NOTES)"' ${UPDATER_JSON_FILE}) >${UPDATER_JSON_FILE}
+cat <<<$(jq --arg NOW "$NOW" '.pub_date = "\($NOW)"' ${UPDATER_JSON_FILE}) >${UPDATER_JSON_FILE}
 
 # Set the linux specific data
-cat <<< $(jq --arg APPIMAGE_URL "$APPIMAGE_URL" '.platforms."linux-x86_64".url = "\($APPIMAGE_URL)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
-cat <<< $(jq --arg APPIMAGE_SIG "$APPIMAGE_SIG" '.platforms."linux-x86_64".signature = "\($APPIMAGE_SIG)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
+cat <<<$(jq --arg APPIMAGE_URL "$APPIMAGE_URL" '.platforms."linux-x86_64".url = "\($APPIMAGE_URL)"' ${UPDATER_JSON_FILE}) >${UPDATER_JSON_FILE}
+cat <<<$(jq --arg APPIMAGE_SIG "$APPIMAGE_SIG" '.platforms."linux-x86_64".signature = "\($APPIMAGE_SIG)"' ${UPDATER_JSON_FILE}) >${UPDATER_JSON_FILE}
 
 # Set the windows specific data
-cat <<< $(jq --arg MSI_URL "$MSI_URL" '.platforms."windows-x86_64".url = "\($MSI_URL)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
-cat <<< $(jq --arg MSI_SIG "$MSI_SIG" '.platforms."windows-x86_64".signature = "\($MSI_SIG)"' ${UPDATER_JSON_FILE}) > ${UPDATER_JSON_FILE}
+cat <<<$(jq --arg MSI_URL "$MSI_URL" '.platforms."windows-x86_64".url = "\($MSI_URL)"' ${UPDATER_JSON_FILE}) >${UPDATER_JSON_FILE}
+cat <<<$(jq --arg MSI_SIG "$MSI_SIG" '.platforms."windows-x86_64".signature = "\($MSI_SIG)"' ${UPDATER_JSON_FILE}) >${UPDATER_JSON_FILE}
 
 # Print the final json file
 cat "${UPDATER_JSON_FILE}"
