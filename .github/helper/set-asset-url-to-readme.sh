293c6cc1fbd10009b150075c77b08737c2017615
Rouven Himmelstein <rouven@himmelstein.info>
2023-09-13 15:12:44 +0200

    fix(pipeline): broken readme update

--- .github/helper/set-asset-url-to-readme.sh
+++ .github/helper/set-asset-url-to-readme.sh
@@ -16,8 +16,8 @@
 set -e
 
 FILE_TO_EDIT="README.md"
-NEW_LINUX_ASSET_URL=$2
-NEW_WINDOWS_ASSET_URL=$3
+NEW_LINUX_ASSET_URL=$1
+NEW_WINDOWS_ASSET_URL=$2
 
 LINUX_HTML_TAG_ID="sensor-bridge-download-linux"
 WINDOWS_HTML_TAG_ID="sensor-bridge-download-windows"
@@ -25,6 +25,17 @@ WINDOWS_HTML_TAG_ID="sensor-bridge-download-windows"
 # Ensure README.md exists
 if [ ! -f "${FILE_TO_EDIT}" ]; then
   echo "Error: ${FILE_TO_EDIT} does not exist!"
+  exit 1
+fi
+
+# Ensure URLS HEAD http response code is smaller than 400
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "${NEW_LINUX_ASSET_URL}")" -ge 400 ]; then
+  echo "Error: ${NEW_LINUX_ASSET_URL} is not reachable!"
+  exit 1
+fi
+
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "${NEW_WINDOWS_ASSET_URL}")" -ge 400 ]; then
+  echo "Error: ${NEW_WINDOWS_ASSET_URL} is not reachable!"
   exit 1
 fi
 
